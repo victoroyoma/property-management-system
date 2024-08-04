f6f02196c2e4d9231bc4d3825b4e2be1c50c3e12
Ogheneyoma Victor <user@Eltov-Air.lan>
2024-08-04 21:18:30 +0100

    prop demo

---
+++ routes/api.php
@@ -0,0 +1,5 @@
+Route::apiResource('properties', PropertyController::class);
+Route::apiResource('tenants', TenantController::class);
+Route::apiResource('maintenance-requests', MaintenanceRequestController::class);
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('users', UserController::class);
